1faafee256f947ca5feac8f3321f9e0845552e1c
Azimjon <azimjonxolmirzayev30@gmail.com>
2026-04-16 15:54:30 +0500

    fix payment

--- backend/src/payments/payments.controller.ts
+++ backend/src/payments/payments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, UseGuards, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards, BadRequestException, Patch } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
